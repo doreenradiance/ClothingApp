94c35d2d455dee3e365691f766c8a18faa10e277
Doreen Mensah <doreenradiance@gmail.com>
2021-05-25 17:21:39 +0000

    wip

--- src/screens/DetailedPage.js
+++ src/screens/DetailedPage.js
@@ -30,25 +30,29 @@ function DetailedPage({ navigation }) {
             <Text style={{ backgroundColor: "#d6d4ce", height: 1, marginTop: 10 }}></Text>
             <Text style={{ fontWeight: "bold", fontSize: 20, marginTop: 10, marginLeft: 10 }}>Price Details</Text>
 
-<View style={{marginLeft:10}}>
-            <View style={{flexDirection:"row"}}>
-                <Text style={{  fontSize: 20 }}>Item Total</Text>
-                <Text style={{alignSelf:"center",justifyContent:"space-between"}}>GHC 25.00</Text>
+            <View style={{ marginLeft: 10 }}>
+                <View style={{ flexDirection: "row" }}>
+                    <Text style={{ fontSize: 20 }}>Item Total</Text>
+                    <Text style={{ alignSelf: "center", justifyContent: "space-between" }}>GHC 25.00</Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+                    <Text style={{ fontSize: 20 }}>Delivery Charges</Text>
+                    <Text style={{ alignSelf: "center", justifyContent: "space-between" }}>GHC 15.00</Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+                    <Text style={{ fontSize: 20 }}>Tax</Text>
+                    <Text style={{ alignSelf: "center", justifyContent: "space-between" }}>GHC 2.00</Text>
+                </View>
+                <Text style={{ backgroundColor: "#d6d4ce", height: 1, marginTop: 10 }}></Text>
+                <View style={{ flexDirection: "row" }}>
+                    <Text style={{ fontSize: 20 }}>Total Amount</Text>
+                    <Text style={{ alignSelf: "center", justifyContent: "space-between" }}>GHC 42.00</Text>
+                </View>
+                <Text style={{ backgroundColor: "#d6d4ce", height: 1, marginTop: 10 }}></Text>
             </View>
-            <View style={{flexDirection:"row"}}>
-                <Text style={{  fontSize: 20 }}>Delivery Charges</Text>
-                <Text style={{alignSelf:"center",justifyContent:"space-between"}}>GHC 15.00</Text>
-            </View>
-            <View style={{flexDirection:"row"}}>
-                <Text style={{  fontSize: 20 }}>Tax</Text>
-                <Text style={{alignSelf:"center",justifyContent:"space-between"}}>GHC 2.00</Text>
-            </View>
-            <Text style={{ backgroundColor: "#d6d4ce", height: 1, marginTop: 10 }}></Text>
-            <View style={{flexDirection:"row"}}>
-                <Text style={{  fontSize: 20 }}>Total Amount</Text>
-                <Text style={{alignSelf:"center",justifyContent:"space-between"}}>GHC 42.00</Text>
-            </View>
-            <Text style={{ backgroundColor: "#d6d4ce", height: 1, marginTop: 10 }}></Text>
+
+            <View style={{ marginLeft: 30, marginTop: 35 }}>
+                <Text style={{ backgroundColor: "#db4460", height: 40, width: 300, borderRadius: 30, textAlign: "center" }}>Proceed to pay</Text>
             </View>
         </View>
     )
