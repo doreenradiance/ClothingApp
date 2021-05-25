628b99d69c966dd764b568b6c2c88f0e6080d139
Doreen Mensah <doreenradiance@gmail.com>
2021-05-25 12:34:02 +0000

    Imported and created navigationContainer and stackNavigation

--- App.js
+++ App.js
@@ -1,14 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+import WelcomePage from "./src/screens/WelcomePage";
+import Home from "./src/screens/HomePage";
+import DetailedPage from "./src/screens/DetailedPage";
+import ProfilePage from "./src/screens/ProfilePage";
+
+const Stack = createStackNavigator()
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen
+          options={{
+            header: () => null
+          }}
+          name="Welcome" component={WelcomePage}
+        />
+
+        {/* <Stack.Screen
+          options={{
+            header: () => null
+          }}
+          name="Home" component={HomePage}
+        />
+
+        <Stack.Screen
+          options={{
+            header: () => null
+          }}
+          name="Detailed" component={DetailedPage}
+        />
+
+        <Stack.Screen
+          options={{
+            header: () => null
+          }}
+          name="Profile" component={ProfilePage}
+        /> */}
+      </Stack.Navigator>
+
+
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
