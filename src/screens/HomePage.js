446b9bc013609f7cfd9516bd71f0dbaf43dd6d27
Doreen Mensah <doreenradiance@gmail.com>
2021-05-25 16:46:10 +0000

    Added the shop now text button

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -8,10 +8,10 @@ function HomePage({ navigation }) {
     return (
         <View style={styles.container}>
             <View style={{ flexDirection: "row", justifyContent: "space-around", marginTop: 30 }}>
-            <TouchableOpacity  onPress={() => {
+                <TouchableOpacity onPress={() => {
                     navigation.navigate("Welcome")
                 }}>
-                <Image source={require('../../assets/logo.jpeg')} style={{ height: 60, width: 60, }} />
+                    <Image source={require('../../assets/logo.jpeg')} style={{ height: 60, width: 60, }} />
                 </TouchableOpacity>
                 <Text style={{ fontSize: 30, fontWeight: "bold" }}>Home</Text>
                 <TouchableOpacity
@@ -27,54 +27,84 @@ function HomePage({ navigation }) {
 
             <ScrollView>
                 <View style={{ flexDirection: "row", justifyContent: "space-around", }}>
-                    <View>
-                        <Image source={require('../../assets/dress.jpg')} style={{ height: 180, width: 150,marginTop: 20 }} />
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Detailed")
+                        }}
+                    >
+                        <Image source={require('../../assets/dress.jpg')} style={{ height: 180, width: 150, marginTop: 20 }} />
                         <Text style={{ fontWeight: "bold", fontSize: 20 }}>Dress</Text>
                         <Text>Ghc 25.00</Text>
-                    </View>
+                        <Text style={{ backgroundColor: "#6788f0", borderRadius: 10, width: 150, height: 40, textAlign: "center" }}>Shop Now</Text>
+                    </TouchableOpacity>
 
-                    <Text style={{ backgroundColor: "#d6d4ce", width: 1, height:"100%" }}></Text>
+                    <Text style={{ backgroundColor: "#d6d4ce", width: 1, height: "100%" }}></Text>
 
-                    <View>
-                        <Image source={require('../../assets/ligerie.jpg')} style={{ height: 180, width: 150,marginTop: 20 }} />
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Detailed")
+                        }}
+                    >
+                        <Image source={require('../../assets/ligerie.jpg')} style={{ height: 180, width: 150, marginTop: 20 }} />
                         <Text style={{ fontWeight: "bold", fontSize: 20 }}>Ligerie</Text>
                         <Text>Ghc 100.00</Text>
-                    </View>
+                        <Text style={{ backgroundColor: "#6788f0", borderRadius: 10, width: 150, height: 40, textAlign: "center" }}>Shop Now</Text>
+                    </TouchableOpacity>
                 </View>
 
                 <Text style={{ backgroundColor: "#d6d4ce", height: 1, }}></Text>
-            
+
                 <View style={{ flexDirection: "row", justifyContent: "space-around", }}>
-                    <View>
-                        <Image source={require('../../assets/tanktop.jpg')} style={{ height: 180, width: 150,marginTop: 20 }} />
+                <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Detailed")
+                        }}
+                    >
+                        <Image source={require('../../assets/tanktop.jpg')} style={{ height: 180, width: 150, marginTop: 20 }} />
                         <Text style={{ fontWeight: "bold", fontSize: 20 }}>Tank Top</Text>
                         <Text>Ghc 25.00</Text>
-                    </View>
+                        <Text style={{ backgroundColor: "#6788f0", borderRadius: 10, width: 150, height: 40, textAlign: "center" }}>Shop Now</Text>
+                    </TouchableOpacity>
 
-                    <Text style={{ backgroundColor: "#d6d4ce", width: 1, height:"100%" }}></Text>
+                    <Text style={{ backgroundColor: "#d6d4ce", width: 1, height: "100%" }}></Text>
 
-                    <View>
-                        <Image source={require('../../assets/womenblouse.jpg')} style={{ height: 180, width: 150,marginTop: 20 }} />
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Detailed")
+                        }}
+                    >
+                        <Image source={require('../../assets/womenblouse.jpg')} style={{ height: 180, width: 150, marginTop: 20 }} />
                         <Text style={{ fontWeight: "bold", fontSize: 20 }}>Women Blouse</Text>
                         <Text>Ghc 30.00</Text>
-                    </View>
+                        <Text style={{ backgroundColor: "#6788f0", borderRadius: 10, width: 150, height: 40, textAlign: "center" }}>Shop Now</Text>
+                    </TouchableOpacity>
                 </View>
                 <Text style={{ backgroundColor: "#d6d4ce", height: 1, }}></Text>
 
                 <View style={{ flexDirection: "row", justifyContent: "space-around", }}>
-                    <View>
-                        <Image source={require('../../assets/womentrouser.jpg')} style={{ height: 180, width: 150,marginTop: 20 }} />
+                <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Detailed")
+                        }}
+                    >
+                        <Image source={require('../../assets/womentrouser.jpg')} style={{ height: 180, width: 150, marginTop: 20 }} />
                         <Text style={{ fontWeight: "bold", fontSize: 20 }}>Trouser</Text>
                         <Text>Ghc 65.00</Text>
-                    </View>
+                        <Text style={{ backgroundColor: "#6788f0", borderRadius: 10, width: 150, height: 40, textAlign: "center" }}>Shop Now</Text>
+                    </TouchableOpacity>
 
-                    <Text style={{ backgroundColor: "#d6d4ce", width: 1, height:"100%" }}></Text>
+                    <Text style={{ backgroundColor: "#d6d4ce", width: 1, height: "100%" }}></Text>
 
-                    <View>
-                        <Image source={require('../../assets/womenskirt.jpg')} style={{ height: 180, width: 150,marginTop: 20 }} />
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Detailed")
+                        }}
+                    >
+                        <Image source={require('../../assets/womenskirt.jpg')} style={{ height: 180, width: 150, marginTop: 20 }} />
                         <Text style={{ fontWeight: "bold", fontSize: 20 }}>Skirt</Text>
                         <Text>Ghc 20.00</Text>
-                    </View>
+                        <Text style={{ backgroundColor: "#6788f0", borderRadius: 10, width: 150, height: 40, textAlign: "center" }}>Shop Now</Text>
+                        </TouchableOpacity>
                 </View>
             </ScrollView>
         </View>
