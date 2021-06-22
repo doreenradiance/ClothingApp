d8c0515cbee13400c1a74bbdaf3acfb097a2f794
Doreen Mensah <doreenradiance@gmail.com>
2021-06-22 20:25:27 +0000

    Uncommented the the whole view of the screen

--- src/components/SingleType.js
+++ src/components/SingleType.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
-import { TouchableOpacity,ScrollView } from 'react-native-gesture-handler'
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { TouchableOpacity,ScrollView } from 'react-native-gesture-handler';
 
-function SingleType({ navigation }) {
+function SingleType ({image,type,price,button,navigation}) {
     return (
 
         <ScrollView>
             <View style={styles.container}>
                 <TouchableOpacity
                     onPress={() => {
-                        navigation.navigate("Detailed")
+                        navigation.navigate("Detailed", {image,type,price,button})
                     }}
                 >
-                    <Image source={require('../../assets/dress.jpg')} style={{ height: 100, width: 90, marginTop: 20 }} />
-                    <Text style={{ fontWeight: "bold", fontSize: 20 }}>Dress</Text>
-                    <Text>Ghc 25.00</Text>
-                    <Text style={{ color: "white", backgroundColor: "#6788f0", borderRadius: 10, width: 90, height: 20, textAlign: "center" }}>Shop Now</Text>
+                    <Image source={image} style={{ height: 100, width: 90, marginTop: 20 }} />
+                    <Text  style={{ fontWeight: "bold", fontSize: 20 }}>{type}</Text>
+                    <Text>{price}</Text>
+                    <Text style={{ color: "white", backgroundColor: "#6788f0", borderRadius: 10, width: 90, height: 20, textAlign: "center" }}>{button}</Text>
                 </TouchableOpacity>
 
                 <Text style={{ backgroundColor: "#d6d4ce", width: 1, height: "100%" }}></Text>
 
                 <TouchableOpacity
                     onPress={() => {
-                        navigation.navigate("Detailed")
+                        navigation.navigate("Detailed",{image,type,price,button})
                     }}
                 >
-                    <Image source={require('../../assets/ligerie.jpg')} style={{ height: 100, width: 90, marginTop: 20 }} />
-                    <Text style={{ fontWeight: "bold", fontSize: 20 }}>Ligerie</Text>
-                    <Text>Ghc 100.00</Text>
-                    <Text style={{ color: "white", backgroundColor: "#6788f0", borderRadius: 10, width: 90, height: 20, textAlign: "center" }}>Shop Now</Text>
+                    <Image source={image} style={{ height: 100, width: 90, marginTop: 20 }} />
+                    <Text style={{ fontWeight: "bold", fontSize: 20 }}>{type}</Text>
+                    <Text>{price}</Text>
+                    <Text style={{ color: "white", backgroundColor: "#6788f0", borderRadius: 10, width: 90, height: 20, textAlign: "center" }}>{button}</Text>
                 </TouchableOpacity>
             </View>
 
