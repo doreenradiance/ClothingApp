47a8302af14dd49055be2a9ed52b29258dabb86b
Doreen Mensah <doreenradiance@gmail.com>
2021-07-20 09:35:04 +0000

    Added new styles

--- src/components/SingleType.js
+++ src/components/SingleType.js
@@ -1,38 +1,27 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
-import { TouchableOpacity,ScrollView } from 'react-native-gesture-handler';
+import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 
-function SingleType ({image,type,price,button,navigation}) {
+function SingleType({ image, type, price, button, navigation }) {
     return (
 
         <ScrollView>
             <View style={styles.container}>
                 <TouchableOpacity
                     onPress={() => {
-                        navigation.navigate("Detailed", {image,type,price,button})
+                        navigation.navigate("Detailed", { image, type, price, button })
                     }}
+                    style={{ maxWidth: 100, minWidth: 100 }}
                 >
                     <Image source={image} style={{ height: 120, width: 100, marginTop: 20 }} />
-                    <Text  style={{ fontWeight: "bold", fontSize: 20 }}>{type}</Text>
-                    <Text>{price}</Text>
-                    <Text style={{ color: "white", backgroundColor: "#6788f0", borderRadius: 10, width: 90, height: 20, textAlign: "center" }}>{button}</Text>
-                </TouchableOpacity>
-
-                {/* <Text style={{ backgroundColor: "#d6d4ce", width: 1, height: "100%" }}></Text> */}
-
-                {/* <TouchableOpacity
-                    onPress={() => {
-                        navigation.navigate("Detailed",{image,type,price,button})
-                    }}
-                >
-                    <Image source={image} style={{ height: 100, width: 90, marginTop: 20 }} />
                     <Text style={{ fontWeight: "bold", fontSize: 20 }}>{type}</Text>
                     <Text>{price}</Text>
-                    <Text style={{ color: "white", backgroundColor: "#6788f0", borderRadius: 10, width: 90, height: 20, textAlign: "center" }}>{button}</Text>
-                </TouchableOpacity> */}
+                    <Text style={{ color: "white",marginBottom:10, backgroundColor: "#6788f0", borderRadius: 10, width: 90, height: 22, textAlign: "center" }}>{button}</Text>
+                </TouchableOpacity>
+                <Text style={{ backgroundColor: "#d6d4ce", width: 1,  }}></Text> 
             </View>
-
-            {/* <Text style={{ backgroundColor: "#d6d4ce", height: 1, marginTop: 10 }}></Text> */}
+         
+        <Text style={{ backgroundColor: "#d6d4ce", height: 1,  }}></Text> 
         </ScrollView>
 
 
@@ -42,6 +31,7 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
         justifyContent: "space-around",
+        flexWrap: "wrap"
     }
 })
 
