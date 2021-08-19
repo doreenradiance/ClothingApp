43043b64aa3b0f38e8d6158015996c1c8ac6c420
Doreen Mensah <doreenradiance@gmail.com>
2021-08-19 16:25:20 +0000

    Created the loginscreen

---
+++ src/screens/LoginPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+class LoginPage extends Component {
+    render() {
+        return (
+            <View>
+                <View>
+                    <Text>Login</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+export default LoginPage;
