525a6f94813907663ace042b649e794ff466ad3d
Doreen Mensah <doreenradiance@gmail.com>
2021-08-20 10:15:09 +0000

    wip

---
+++ src/screens/SignupLogin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SignupLogin extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Signup or login to purchase or order.</Text>
+            </View>
+        )
+    }
+}
+
+export default SignupLogin
