0cd1269baf0b8464b8ff9009de4fc0183e9a5f9e
Doreen Mensah <doreenradiance@gmail.com>
2021-08-20 12:38:50 +0000

    Changed the color of the button

--- src/components/SingleType.js
+++ src/components/SingleType.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
-import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
+import { TouchableOpacity, } from 'react-native-gesture-handler';
 
 function SingleType({ image, type, price, button, navigation, index }) {
     return (
@@ -16,12 +16,10 @@ function SingleType({ image, type, price, button, navigation, index }) {
                     <Image source={image} style={{ height: 120, width: 100, marginTop: 20 }} />
                     <Text style={{ fontWeight: "bold", fontSize: 20 }}>{type}</Text>
                     <Text>{price}</Text>
-                    <Text style={{ color: "white",marginBottom:10, backgroundColor: "#6788f0", borderRadius: 10, width: 90, height: 22, textAlign: "center" }}>{button}</Text>
+                    <Text style={{ color: "white",marginBottom:10, backgroundColor: "#a85c1e", borderRadius: 10, width: 90, height: 22, textAlign: "center" }}>{button}</Text>
                 </TouchableOpacity>
-                {/* <Text style={{ backgroundColor: "#d6d4ce", width: 2,  }}></Text>  */}
+               
             </View>
-         
-        {/* <Text style={{ backgroundColor: "#d6d4ce", height: 2,  }}></Text> */}
         </View>
 
 
