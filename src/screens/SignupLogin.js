bf2c92a8f01d3c9e31f77b1e5be5823a52837997
Doreen Mensah <doreenradiance@gmail.com>
2021-08-20 12:34:34 +0000

    wip

--- src/screens/SignupLogin.js
+++ src/screens/SignupLogin.js
@@ -1,13 +1,74 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { TouchableOpacity, } from 'react-native-gesture-handler';
 
-class SignupLogin extends Component {
-    render() {
-        return (
-            <View>
-                <Text>Signup or login to purchase or order.</Text>
+function SignupLogin({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={{ alignSelf: "center", fontSize: 20, fontStyle: "italic" }}>Signup or login to purchase or order.</Text>
             </View>
-        )
-    }
+
+            <View style={{ alignItems: "center", marginTop: 70 }}>
+
+                <View style={styles.signupTextButton}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Signup")
+                        }}
+                    >
+                        <Text style={{ fontSize: 18, alignSelf: "center", color: "white", marginTop: 12 }}>Signup</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={styles.loginTextButton}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            navigation.navigate("Login")
+                        }}
+                    >
+                        <Text style={{color:"#a85c1e",alignSelf:"center", marginTop:15}}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    )
 }
 
-export default SignupLogin
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        // flexDirection: "row",
+        // justifyContent: "space-evenly",
+    },
+
+    header: {
+        backgroundColor: "#a85c1e",
+        height: 80,
+        width: "100%",
+        flex: 0.6,
+        justifyContent: "space-around"
+    },
+    signupTextButton: {
+        backgroundColor: "#a85c1e",
+        height: 50,
+        width: 250,
+        textAlign: "center",
+        borderRadius: 4,
+    },
+    loginTextButton: {
+        backgroundColor: "white",
+        height: 50,
+        width: 250,
+        textAlign: "center",
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor: "#a85c1e",
+        marginTop: 50
+    },
+})
+
+export default SignupLogin;
+
+
