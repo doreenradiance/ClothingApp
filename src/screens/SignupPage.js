32b21591030673b88b81b2352ec2ce66caabb11d
Doreen Mensah <doreenradiance@gmail.com>
2021-08-21 14:40:57 +0000

    wip

--- src/screens/SignupPage.js
+++ src/screens/SignupPage.js
@@ -1,33 +1,36 @@
 import React from 'react'
-import { View,Image,Label, TextInput, Text} from 'react-native'
+import { View, Text, Image, Label, TextInput,StyleSheet } from "react-native"
+
 
 function SignupPage() {
     return (
         <View>
-               <Text>Sign Up</Text>
-               <Image source={require('../../assets/signup.jpg')}/>
-               <Label>Email</Label>
-               <TextInput></TextInput>
-               <View style={{ backgroundColor: "#d6d4ce", height: 2, marginTop: 5 }}></View>
-           </View>
+            <Text style={{alignSelf:"center", marginTop:20,fontSize:30}}>Sign Up</Text>
+            <Image source={require('../../assets/signup.jpg')}  style={{height:150, width:"100%"}} />
+            <Text>Email</Text>
+            <TextInput style={styles.input}
+                placeholderTextColor="#aaaaaa"
+                placeholder="Email"
+                // value={email}
+                // onChangeText={email => setEmail(email)} 
+                />
+            <View style={{ backgroundColor: "#d6d4ce", height: 2, marginTop: 5 }}></View>
+        </View>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        // flexDirection: "row",
+        // justifyContent: "center",
+        
+    },
+    inputs: {
+        // marginTop: 70,
+        alignSelf: "flex-start",
+        marginLeft: 10,
+    },
+})
+
 export default SignupPage
-
-
-// class SignupPage extends Component ({navigation}){
-//     render() {
-//         return (
-//            <View>
-//                <Text>Sign Up</Text>
-//                <Image source={require('../../assets/signup.jpg')}/>
-//                <Label>Email</Label>
-//                <TextInput></TextInput>
-//                <View style={{ backgroundColor: "#d6d4ce", height: 2, marginTop: 5 }}></View>
-//            </View>
-//         )
-//     }
-// }
-
-// export default SignupPage
